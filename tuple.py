e306d88e65d7539696e1cb745e59496975ccab3d
Vinayak <yadavvinayakk@gmail.com>
2024-06-30 00:52:44 +0530

    W

--- tuple.py
+++ tuple.py
@@ -22,20 +22,20 @@ print(colours[-1]) # negative indexing
 print(colours[1:3]) # range indexing 3 is last item to print+1
 print(colours[-2:]) # negative range indexing here we didnt mentioned ending point
 
-# # check if an item exist in tuple
-# if "green" in colours:
-#     print("Green is part of tuple")
+# check if an item exist in tuple
+if "green" in colours:
+    print("Green is part of tuple")
 
-# # traverse the tuple
-# for i in colours:
-#     print(i)
+# traverse the tuple
+for i in colours:
+    print(i)
 
-# # concatinate 2 tuple
-# more_colours= ("blue", "brown")
-# colours = colours + more_colours
-# print(colours)
+# concatinate 2 tuple
+more_colours= ("blue", "brown")
+colours = colours + more_colours
+print(colours)
 
-# unpacking a tuple
-# colour1, colour2, colour3 = colours
-# print(colour1,colour2,colour3)
+#unpacking a tuple
+colour1, colour2, colour3 = colours
+print(colour1,colour2,colour3)
 
